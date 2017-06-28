edb3202d0691d0faf2520d87d9d113371e196895
Yimeng Zhu <yimeng.zhu0821@gmail.com>
2017-06-28 12:06:52 +0200

    added sensorClient.py to collect data from sensor source

---
+++ sensorClient.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+class SensorVal
+	'Measurment raw data from a sensor'
+
+	def __init__(self, value)
+		self.value = value,
+		self.time = datetime.now().time()
+
+
+		
+
+class GPSVal
+
+ 

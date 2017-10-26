9932cdd7caa1ef25bc2d4fcad1696ce6170d015d
Yimeng Zhu <yimeng.zhu0821@gmail.com>
2017-10-26 23:31:16 +0200

    compiled new proto

--- src/test_pb2.py
+++ src/test_pb2.py
@@ -19,49 +19,49 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='test.proto',
   package='test',
   syntax='proto2',
-  serialized_pb=_b('\n\ntest.proto\x12\x04test\"?\n\x06Person\x12\t\n\x01r\x18\x01 \x02(\x02\x12\t\n\x01t\x18\x02 \x02(\x03\x12\t\n\x01x\x18\x03 \x02(\x02\x12\t\n\x01y\x18\x04 \x02(\x02\x12\t\n\x01\x64\x18\x05 \x02(\x05')
+  serialized_pb=_b('\n\ntest.proto\x12\x04test\"R\n\x19ObservationWithCoordinate\x12\t\n\x01r\x18\x01 \x02(\x02\x12\t\n\x01t\x18\x02 \x02(\x03\x12\t\n\x01x\x18\x03 \x02(\x02\x12\t\n\x01y\x18\x04 \x02(\x02\x12\t\n\x01\x64\x18\x05 \x02(\x05\"A\n\x13ObservationWithGrid\x12\t\n\x01r\x18\x06 \x02(\x02\x12\t\n\x01t\x18\x07 \x02(\x03\x12\t\n\x01g\x18\x08 \x02(\x0c\x12\t\n\x01\x64\x18\t \x02(\x05')
 )
 
 
 
 
-_PERSON = _descriptor.Descriptor(
-  name='Person',
-  full_name='test.Person',
+_OBSERVATIONWITHCOORDINATE = _descriptor.Descriptor(
+  name='ObservationWithCoordinate',
+  full_name='test.ObservationWithCoordinate',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='r', full_name='test.Person.r', index=0,
+      name='r', full_name='test.ObservationWithCoordinate.r', index=0,
       number=1, type=2, cpp_type=6, label=2,
       has_default_value=False, default_value=float(0),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='t', full_name='test.Person.t', index=1,
+      name='t', full_name='test.ObservationWithCoordinate.t', index=1,
       number=2, type=3, cpp_type=2, label=2,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='x', full_name='test.Person.x', index=2,
+      name='x', full_name='test.ObservationWithCoordinate.x', index=2,
       number=3, type=2, cpp_type=6, label=2,
       has_default_value=False, default_value=float(0),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='y', full_name='test.Person.y', index=3,
+      name='y', full_name='test.ObservationWithCoordinate.y', index=3,
       number=4, type=2, cpp_type=6, label=2,
       has_default_value=False, default_value=float(0),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='d', full_name='test.Person.d', index=4,
+      name='d', full_name='test.ObservationWithCoordinate.d', index=4,
       number=5, type=5, cpp_type=1, label=2,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
@@ -80,18 +80,78 @@ _PERSON = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=20,
-  serialized_end=83,
+  serialized_end=102,
 )
 
-DESCRIPTOR.message_types_by_name['Person'] = _PERSON
+
+_OBSERVATIONWITHGRID = _descriptor.Descriptor(
+  name='ObservationWithGrid',
+  full_name='test.ObservationWithGrid',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='r', full_name='test.ObservationWithGrid.r', index=0,
+      number=6, type=2, cpp_type=6, label=2,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='t', full_name='test.ObservationWithGrid.t', index=1,
+      number=7, type=3, cpp_type=2, label=2,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='g', full_name='test.ObservationWithGrid.g', index=2,
+      number=8, type=12, cpp_type=9, label=2,
+      has_default_value=False, default_value=_b(""),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='d', full_name='test.ObservationWithGrid.d', index=3,
+      number=9, type=5, cpp_type=1, label=2,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto2',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=104,
+  serialized_end=169,
+)
+
+DESCRIPTOR.message_types_by_name['ObservationWithCoordinate'] = _OBSERVATIONWITHCOORDINATE
+DESCRIPTOR.message_types_by_name['ObservationWithGrid'] = _OBSERVATIONWITHGRID
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
-Person = _reflection.GeneratedProtocolMessageType('Person', (_message.Message,), dict(
-  DESCRIPTOR = _PERSON,
+ObservationWithCoordinate = _reflection.GeneratedProtocolMessageType('ObservationWithCoordinate', (_message.Message,), dict(
+  DESCRIPTOR = _OBSERVATIONWITHCOORDINATE,
   __module__ = 'test_pb2'
-  # @@protoc_insertion_point(class_scope:test.Person)
+  # @@protoc_insertion_point(class_scope:test.ObservationWithCoordinate)
   ))
-_sym_db.RegisterMessage(Person)
+_sym_db.RegisterMessage(ObservationWithCoordinate)
+
+ObservationWithGrid = _reflection.GeneratedProtocolMessageType('ObservationWithGrid', (_message.Message,), dict(
+  DESCRIPTOR = _OBSERVATIONWITHGRID,
+  __module__ = 'test_pb2'
+  # @@protoc_insertion_point(class_scope:test.ObservationWithGrid)
+  ))
+_sym_db.RegisterMessage(ObservationWithGrid)
 
 
 # @@protoc_insertion_point(module_scope)
